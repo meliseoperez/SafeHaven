48909ccad30f1ad1907ae27fbddcdcf70242036f
meliseoperez <meperezlavandeira@gmail.com>
2023-11-01 20:36:03 +0100

    se han elimado los marker del mapa, se han modificado métodos para el procesamiento de coordenadas y se ha añadido la extracción del nivel de alerta para representar el color en el mapa, se han añadido atributos a la clase AlertInfo, así como métodos set y get.

--- app/src/main/java/es/meliseoperez/MainActivity.java
+++ app/src/main/java/es/meliseoperez/MainActivity.java
@@ -5,14 +5,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.SQLException;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
-import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.File;
@@ -31,128 +29,122 @@ import es.meliseoperez.safehaven.api.googlemaps.CustomMapsFragment;
 import es.meliseoperez.safehaven.api.googlemaps.Zona;
 
 import es.meliseoperez.safehaven.database.AlertRepository;
-import es.meliseoperez.safehaven.pruebabbdd.DisplayDatabaseActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private CustomMapsFragment customMapsFragment;
+    private AlertRepository alertRepository;
+    private List<AlertInfo> listaAlertas;
 
-        // Variables para el manejo de la base de datos y la lista de alertas.
+    private static final int LOCATION_REQUEST_CODE = 101;
 
-        AlertRepository alertRepository;
-        List<AlertInfo> listaAlertas;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Establezco el contenido de la vista desde el layout del recurso XML.
+        setContentView(R.layout.activity_main);
 
-        // Constantes y herramientas para el manejo de hilos y permisos.
-        private static final String TAG = "MainActivity";
-        private final ExecutorService executorService = Executors.newSingleThreadExecutor();
-        private final Handler handler = new Handler(Looper.getMainLooper());
+        // Verifica y elimina la base de datos si ya existe.
+        checkAndDeleteDatabase();
 
-        public static List<String> polygonList;
-        private static final int LOCATION_REQUEST_CODE=101;
+        // Inicio la transacción para incluir el mapa personalizado en la UI.
+        setupUI();
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            // Establezco el contenido de la vista desde el layout del recurso XML.
-            setContentView(R.layout.activity_main);
+        // Solicito permisos de ubicación, ya que mi aplicación los necesita para su funcionalidad esencial.
+        requestLocationPermission();
+        // Inicio la recuperación de datos desde la API.
+        fetchDataFromApi();
+    }
 
-            // Inicio la recuperación de datos desde la API.
-            fetchDataFromApi();
+    private void setupUI(){
+        customMapsFragment=new CustomMapsFragment();
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container,customMapsFragment);
+        fragmentTransaction.commit();
+    }
 
-            // Verifica y elimina la base de datos si ya existe.
-            checkAndDeleteDatabase();
-            // Preparo el sistema de gestión de la base de datos.
-
-            // Inicio la transacción para incluir el mapa personalizado en la UI.
-            CustomMapsFragment customMapsFragment = new CustomMapsFragment();
-            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, customMapsFragment);
-            fragmentTransaction.commit();
-
-            // Solicito permisos de ubicación, ya que mi aplicación los necesita para su funcionalidad esencial.
-            requestLocationPermission();
-
-
-
-
-            // Configuro el botón en la UI para que el usuario también pueda iniciar la descarga de datos.
-            Button fetchApiDataButton = findViewById(R.id.fetchApiDataButton);
-            fetchApiDataButton.setOnClickListener(v -> {
-                polygonList=customMapsFragment.cargarZonas(alertRepository);
-                List<Zona> zonas=new ArrayList<>();
-                int contador = 1; // Un contador para asignar nombres a las zonas de forma dinámica
-            for (String coordenada : polygonList) {
-                Zona zona = new Zona(coordenada, "Zona " + contador); // Creando una nueva Zona con nombre dinámico
-                zonas.add(zona); // Agregando la Zona a la lista
-                contador++;
-           }
-                customMapsFragment.addZonesToMap(zonas);
-
-            });
-            Button showData = findViewById(R.id.showData);
-                showData.setOnClickListener(view -> {
-                    Intent intent=new Intent(MainActivity.this, DisplayDatabaseActivity.class);
-                    startActivity(intent);
-            });
+    private void requestLocationPermission() {
+        // Verifico si los permisos ya están otorgados; de lo contrario, solicito explícitamente.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
         }
+    }
 
-        private void requestLocationPermission() {
-            // Verifico si los permisos ya están otorgados; de lo contrario, solicito explícitamente.
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
-            }
-        }
+    private void insertAlertsIntoDatabase(List<AlertInfo> listaAlertas) {
+        // Establezco conexión con la base de datos y preparo para la inserción de datos.
+        alertRepository = new AlertRepository(MainActivity.this);
+        try {
+            alertRepository.open();
 
-        private void insertAlertsIntoDatabase(List<AlertInfo> listaAlertas) {
-            // Establezco conexión con la base de datos y preparo para la inserción de datos.
-            alertRepository = new AlertRepository(MainActivity.this);
-            try {
-                alertRepository.open();
-
-                // Inserto cada alerta en la base de datos, registrando cualquier problema en el log.
-                for (AlertInfo alerta : listaAlertas) {
-                    long id = alertRepository.insertAlert(alerta);
-                    if (id == -1) {
-                        Log.e(TAG, "Error al insertar alerta: " + alerta);
-                    }
+            // Inserto cada alerta en la base de datos, registrando cualquier problema en el log.
+            for (AlertInfo alerta : listaAlertas) {
+                long id = alertRepository.insertAlert(alerta);
+                if (id == -1) {
+                    Log.e(TAG, "Error al insertar alerta: " + alerta);
                 }
-            } catch (SQLException e) {
-                Log.e(TAG, "Error en la base de datos", e);
-            } finally {
-                alertRepository.close(); // Siempre aseguro el cierre de la base de datos.
             }
+        } catch (SQLException e) {
+            Log.e(TAG, "Error en la base de datos", e);
+        } finally {
+            alertRepository.close(); // Siempre aseguro el cierre de la base de datos.
         }
+    }
+    private void fetchDataFromApi(){
+        Runnable fectchDataRunnable=()->{
+            try{
+                processApiData();
+                handler.post(()->Toast.makeText(MainActivity.this,"Datos procesados correctamente", Toast.LENGTH_LONG).show());
+            }catch (Exception e){
+                handler.post(()->Toast.makeText(MainActivity.this,"Error al obtener datos: "+ e.getMessage(),Toast.LENGTH_LONG).show());
+            }
+        };
+        executorService.execute(fectchDataRunnable);
+    }
 
-        private void fetchDataFromApi() {
-            // Defino una tarea para el hilo secundario, evitando sobrecargar el hilo principal.
-            Runnable fetchDataRunnable = () -> {
-                try {
-                    // Proceso de descarga y manipulación de datos.
-                    DownloadAndStoreXMLAlerts dataDownloader = new DownloadAndStoreXMLAlerts();
-                    dataDownloader.downloadData(new MyCallBack() {
-                        @Override
-                        public void onCompleted() {
-                            AlertXMLHandler alertXMLHandler = new AlertXMLHandler(MainActivity.this);
-                            alertXMLHandler.processAndSaveXML(new MyCallBack() {
-                                @Override
-                                public void onCompleted() {
-                                    AlertsExtractor alertsExtractor = new AlertsExtractor(MainActivity.this);
-                                    listaAlertas = alertsExtractor.extractAlertsInfo();
-                                    insertAlertsIntoDatabase(listaAlertas); // Almacenamiento en DB.
+    private void processApiData() {
+        DownloadAndStoreXMLAlerts dataDownloader=new DownloadAndStoreXMLAlerts();
+        dataDownloader.downloadData(new MyCallBack() {
+            @Override
+            public void onCompleted() {
+                handleDownloadCompletion();
+            }
+        },MainActivity.this);
+    }
+    private void handleDownloadCompletion(){
+        AlertXMLHandler alertXMLHandler=new AlertXMLHandler(MainActivity.this);
+        alertXMLHandler.processAndSaveXML(new MyCallBack() {
+            @Override
+            public void onCompleted() {
+                processAlertsAndDisplayOnMap();
+            }
+        });
+    }
 
-                                }
-                            });
-                        }
-                    }, MainActivity.this);
+    private void processAlertsAndDisplayOnMap() {
+        AlertsExtractor alertsExtractor=new AlertsExtractor(MainActivity.this);
+        listaAlertas = alertsExtractor.extractAlertsInfo();
+        insertAlertsIntoDatabase(listaAlertas);
+        loadZonesOnMap();
+    }
 
-                    // Informo al usuario sobre el estado de la tarea.
-                    handler.post(() -> Toast.makeText(MainActivity.this, "Datos procesados correctamente", Toast.LENGTH_LONG).show());
-                } catch (Exception e) {
-                    handler.post(() -> Toast.makeText(MainActivity.this, "Error al obtener datos: " + e.getMessage(), Toast.LENGTH_LONG).show());
-                }
-            };
+    private void loadZonesOnMap() {
+        List<AlertInfo> alertas= customMapsFragment.cargarZonas(alertRepository);
 
-            // Ejecuto la tarea en el hilo secundario.
-            executorService.execute(fetchDataRunnable);
+
+
+        //polygonList=customMapsFragment.cargarZonas(alertRepository);
+        List<Zona> zonas=new ArrayList<>();
+        int contador=1;
+        for(AlertInfo alerta: alertas){
+            Zona zona= new Zona(alerta.getCoordenadas(),alerta.getColor());
+            zonas.add(zona);
+            contador++;
         }
+        customMapsFragment.addZonesToMap(zonas);
+    }
+
     private void checkAndDeleteDatabase() {
         String dbName = "Alerts.db";
         File dbFile = getDatabasePath(dbName);
@@ -165,10 +157,11 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-        @Override
-        protected void onDestroy() {
-            super.onDestroy();
-            // Antes de destruir la actividad, cierro los servicios para evitar fugas de memoria.
-            executorService.shutdown();
-        }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Antes de destruir la actividad, cierro los servicios para evitar fugas de memoria.
+        executorService.shutdown();
+    }
 }
