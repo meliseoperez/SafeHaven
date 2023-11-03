bab4817417b63050097bf519e3c3b714f70008f6
meliseoperez <meperezlavandeira@gmail.com>
2023-11-03 12:10:47 +0100

    feat: carpeta app

--- app/src/main/java/es/meliseoperez/safehaven/api/googlemaps/CustomMapsFragment.java
+++ app/src/main/java/es/meliseoperez/safehaven/api/googlemaps/CustomMapsFragment.java
@@ -31,6 +31,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import es.meliseoperez.safehaven.R;
+import es.meliseoperez.safehaven.api.aemet.AlertInfo;
+import es.meliseoperez.safehaven.database.AlertRepository;
 
 
 public class CustomMapsFragment extends Fragment implements OnMapReadyCallback {
@@ -41,11 +43,10 @@ public class CustomMapsFragment extends Fragment implements OnMapReadyCallback {
     private boolean firsLocationUpdate=true;
     private Marker myLocationMarker;
     private FusedLocationProviderClient fusedLocationProviderClient;
-    private List<Marker> zoneMarkers = new ArrayList<>();
+    private final List<Marker> zoneMarkers = new ArrayList<>();
 
 
-    Zona zona1=new Zona("28.49,-16.43 28.43,-16.39 28.39,-16.44 28.35,-16.48 28.31,-16.5 28.28,-16.54 28.25,-16.55 28.2,-16.64 28.21,-16.68 28.27,-16.7 28.31,-16.79 28.27,-16.85 28.31,-16.88 28.34,-16.92 28.36,-16.92 28.37,-16.86 28.39,-16.83 28.38,-16.81 28.37,-16.75 28.38,-16.7 28.4,-16.67 28.39,-16.6 28.42,-16.55 28.42,-16.52 28.44,-16.47 28.49,-16.43");
-    Zona zona2=new Zona("38.17,-4.17 38.21,-4.2 38.25,-4.22 38.3,-4.21 38.35,-4.27 38.39,-4.28 38.4,-4.26 38.38,-4.16 38.38,-4.07 38.37,-3.96 38.38,-3.85 38.42,-3.81 38.42,-3.73 38.39,-3.62 38.41,-3.58 38.45,-3.58 38.45,-3.54 38.41,-3.53 38.4,-3.47 38.41,-3.42 38.44,-3.38 38.48,-3.37 38.48,-3.3 38.46,-3.28 38.45,-3.18 38.44,-3.13 38.48,-3.06 38.42,-3.01 38.45,-2.99 38.47,-2.96 38.47,-2.9 38.45,-2.88 38.37,-2.91 38.35,-2.94 38.31,-2.94 38.25,-3.0 38.26,-3.03 38.23,-3.07 38.21,-3.05 38.18,-3.1 38.15,-3.18 38.13,-3.2 38.15,-3.3 38.13,-3.34 38.17,-3.37 38.12,-3.4 38.13,-3.44 38.09,-3.48 38.09,-3.56 38.04,-3.59 38.02,-3.63 38.01,-3.68 38.04,-3.68 38.06,-3.73 38.04,-3.76 38.04,-3.9 37.98,-3.9 37.95,-3.91 37.97,-3.95 38.0,-3.96 37.98,-4.04 38.0,-4.06 38.02,-4.13 38.07,-4.1 38.12,-4.08 38.14,-4.11 38.17,-4.17");
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
@@ -113,11 +114,8 @@ public class CustomMapsFragment extends Fragment implements OnMapReadyCallback {
                 locationRequest.setFastestInterval(5000);
                 fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
             }
-            List<Zona> zonas = new ArrayList<>();
-            zonas.add(zona1);
-            zonas.add(zona2);
 
-            addZonesToMap(zonas);
+
         }
     }
 
@@ -135,7 +133,7 @@ public class CustomMapsFragment extends Fragment implements OnMapReadyCallback {
         fusedLocationProviderClient.removeLocationUpdates(locationCallback);
     }
 
-    private void addZonesToMap(List<Zona> zonas) {
+    public void addZonesToMap(List<Zona> zonas) {
         if (mMap == null) return;
 
         for (Marker marker : zoneMarkers) {
@@ -145,9 +143,25 @@ public class CustomMapsFragment extends Fragment implements OnMapReadyCallback {
 
         for (Zona zona : zonas) {
             zona.dibujarZona(mMap);
-            LatLng zoneCenter = zona.getCentro();
-            Marker marker = mMap.addMarker(new MarkerOptions().position(zoneCenter).title("Zona"));
-            zoneMarkers.add(marker);
+
         }
+    }
+    public List<AlertInfo> cargarZonas(AlertRepository repository){
+
+        repository.open();
+        List<AlertInfo> alerts= repository.getAllAlerts();
+        repository.close();
+
+
+        for(AlertInfo alert: alerts){
+            //Procesar y almacenar color
+            String color= Zona.extractColorFromHeadline(alert.getHeadline());
+            alert.setColor(color);
+            //Procesar y almacenar coordenadas
+            List<LatLng> coordenadasProcesadas=Zona.parsearCoordenadas(alert.getPolygon());
+            alert.setCoordenadas(coordenadasProcesadas);
+
+        }
+        return  alerts;
     }
 }
