f45801f0bc4ede5cfa2bc50b5d127595add9dac7
meliesoperez <meperezlavandeira@gmail.com>
2024-03-07 12:15:24 +0100

    solcuión de errores en remoto

--- app/src/main/java/es/meliseoperez/safehaven/login/RegisterActivity.java
+++ app/src/main/java/es/meliseoperez/safehaven/login/RegisterActivity.java
@@ -38,6 +38,7 @@ public class RegisterActivity extends AppCompatActivity {
         nameInput = findViewById(R.id.etName);
         emailInput = findViewById(R.id.etEmail);
         passwordInput = findViewById(R.id.etPassword);
+        //Comprueba identificación layout en remoto.
         typeUserInput = findViewById(R.id.rgTypeUser);
 
         // Escucha el cambio en la selección de RadioGroup
