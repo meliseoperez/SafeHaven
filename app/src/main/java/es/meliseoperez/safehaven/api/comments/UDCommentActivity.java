4fb2297a1382e72182fdde4f9b4408f8200682cc
meliesoperez <meperezlavandeira@gmail.com>
2024-03-22 20:29:30 +0100

    Finalizado desarrollo de vista para modificación y eliminación comentarios.

--- app/src/main/java/es/meliseoperez/safehaven/api/comments/UDCommentActivity.java
+++ app/src/main/java/es/meliseoperez/safehaven/api/comments/UDCommentActivity.java
@@ -1,8 +1,12 @@
 package es.meliseoperez.safehaven.api.comments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,88 +14,161 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
-import com.google.gson.annotations.SerializedName;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.List;
 
 import es.meliseoperez.safehaven.R;
 import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class UDCommentActivity extends AppCompatActivity {
-
+    private Button actualizar;
+    private Button eliminar;
     private TextView textView;
+    private EditText etnuevoComentario;
     private String comentarioUnico;
-    private  SharedPreferences sharedPreferences;
+    private String nuevoComentario;
+    private SharedPreferences sharedPreferences;
+    private int commentId;
+    private int idUsuario;
+
+    private OkHttpClient okHttpClient;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_udcomment);
-        sharedPreferences = getSharedPreferences("mis_preferencias", Context.MODE_PRIVATE);
-        textView = findViewById(R.id.idComent);
-        String iD= String.valueOf(getIntent().getIntExtra("comentarioId",0));
-        //textView.setText(iD);
-        cargaUnComentario(Integer.valueOf(iD));
 
-    }
-    public void cargaUnComentario(Integer idComentario) {
+        sharedPreferences = getSharedPreferences("mis_preferencias", Context.MODE_PRIVATE);
         String token = sharedPreferences.getString("token", "");
-        OkHttpClient client = new OkHttpClient();
-        // Asume que este es el URL para obtener un solo comentario. Ajusta según sea necesario.
-        String url =  "http://10.0.2.2:8000/api/v1/comments/" + idComentario + "?type=idComent";
+        okHttpClient = new OkHttpClient();
+
+        textView = findViewById(R.id.idComent);
+        actualizar = findViewById(R.id.btnModificarComentario);
+        eliminar = findViewById(R.id.btnElinarComentario);
+
+        etnuevoComentario = findViewById(R.id.etnuevoComentario);
+
+        idUsuario = getIntent().getIntExtra("usuarioId",0);
+        commentId = getIntent().getIntExtra("comentarioId", 0);
+
+        cargarComentario(commentId,token);
+        eliminar.setOnClickListener(v-> eliminarMensaje(commentId,token));
+        actualizar.setOnClickListener(v-> modificarMensaje(commentId, token));
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    private void cargarComentario(int commentId, String token){
+       String url = "http://10.0.2.2:8000/api/v1/comments/"+commentId +"?type=idComent";
+       Request request = new Request.Builder()
+               .url(url)
+               .addHeader("Authorization", "Bearer " + token)
+               .build();
+       okHttpClient.newCall(request).enqueue(new Callback() {
+           @Override
+           public void onFailure(@NonNull Call call, @NonNull IOException e) {
+               Toast.makeText(getApplicationContext(),"error en carga comentario antiguo.",Toast.LENGTH_LONG).show();;
+           }
+
+           @Override
+           public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+               String responseData = response.body().string();
+               if (response.isSuccessful()) {
+                   Gson gson = new Gson();
+                   Comentario comentario = gson.fromJson(responseData, ComentariosResponse.class).getData().get(0);
+                   comentarioUnico = comentario.getCommentText();
+                   runOnUiThread(()->{
+                       textView.setText(comentarioUnico);
+                   });
+               } else {
+                   runOnUiThread(() -> Toast.makeText(getApplicationContext(), "Error al cargar el comentario.", Toast.LENGTH_LONG).show());
+               }
+           }
+       });
+    }
+    private void eliminarMensaje(int commentId, String token){
+        String url = "http://10.0.2.2:8000/api/v1/comments/"+commentId;
+        Request request = new Request.Builder()
+                .url(url)
+                .delete()
+                .addHeader("Authorization", "Bearer " + token)
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                runOnUiThread(() -> Toast.makeText(getApplicationContext(), "Error al actualizar el comentario.", Toast.LENGTH_LONG).show());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    runOnUiThread(() ->Toast.makeText(getApplicationContext(),"Comentario ELIMINADO.",Toast.LENGTH_LONG).show());
+                } else {
+                    runOnUiThread(() ->Toast.makeText(getApplicationContext(),"error al ELIMINAR comentario .",Toast.LENGTH_LONG).show());
+                }
+            }
+        });
+    }
+    private void modificarMensaje(int commentId, String token){
+        String  nuevoComentario = etnuevoComentario.getText().toString();
+        if(nuevoComentario == null){
+            Toast.makeText(getApplicationContext(),"El comentario está vacío.",Toast.LENGTH_LONG).show();
+            return;
+        }
+        String url = "http://10.0.2.2:8000/api/v1/comments/"+commentId;
+
+        JSONObject comentarioJson = new JSONObject();
+        try{
+            comentarioJson.put("comment_text",nuevoComentario);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        RequestBody body = RequestBody.create(comentarioJson.toString(), MediaType.parse("application/json; charset=utf-8"));
 
         Request request = new Request.Builder()
                 .url(url)
+                .patch(body)
                 .addHeader("Authorization", "Bearer " + token)
                 .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                runOnUiThread(() -> Toast.makeText(getApplicationContext(), "Error al actualizar el comentario.", Toast.LENGTH_LONG).show());
+            }
 
-        client.newCall(request).enqueue(new okhttp3.Callback() {
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                String responseData = response.body().string();
+
                 if (response.isSuccessful()) {
-                    // Aquí asumimos que la respuesta es un solo comentario y lo almacenamos
-                    Gson gson = new Gson();
-                    Comentario comentario = gson.fromJson(responseData, ResponseWrapper.class).getData().get(0);
-                    comentarioUnico = comentario.getCommentText();
-                    // Aquí podrías actualizar la interfaz de usuario para mostrar el comentario o hacer algo con él
-                    runOnUiThread(() -> {
-                        // Por ejemplo, actualizar un TextView con el comentario
-                         textView.setText(comentarioUnico);
-                    });
+                    runOnUiThread(() -> Toast.makeText(getApplicationContext(), "El comentario ha sido modificado.", Toast.LENGTH_LONG).show());
+                    finish();
                 } else {
-                    showErrorToast("Error al cargar el comentario.");
+                    runOnUiThread(() -> Toast.makeText(getApplicationContext(), "Error al actualizar el comentario.", Toast.LENGTH_LONG).show());
                 }
             }
 
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                showErrorToast("Error de conexión con el servidor.");
-            }
-
-            // runOnUiThread y showErrorToast métodos como antes
         });
     }
-    private void showErrorToast(String message) {
-        // Utiliza el contexto para mostrar el Toast. Asegúrate de ejecutarlo en el hilo principal.
-        ((AppCompatActivity) this).runOnUiThread(() -> Toast.makeText(this, message, Toast.LENGTH_LONG).show());
-    }
-    public class ResponseWrapper {
-        @SerializedName("data")
-        private List<Comentario> data;
-
-        // Getter para la lista de comentarios
-        public List<Comentario> getData() {
-            return data;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == android.R.id.home){
+            finish();
+            Intent intent = new Intent(getApplicationContext(), ComentariosActivity.class);
+            startActivity(intent);
         }
-
-        // Setter para la lista de comentarios (opcional)
-        public void setData(List<Comentario> data) {
-            this.data = data;
-        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
